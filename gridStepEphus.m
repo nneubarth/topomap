df65e83f7a997fc8f2b04632a52bb528d0b2feea
Nicole Neubarth <nicoleneubarth@gmail.com>
2015-01-16 17:47:54 -0500

    added function for interaction with ephus acquisition

---
+++ gridStepEphus.m
@@ -0,0 +1,10 @@
+function [ output_args ] = gridStepEphus( grid,stage )
+%GRIDSTEPEPHUS: to be called from ephus userFcns GUI by
+%the loopManager: iterate event. 
+%input args: Grid object and StageGUI object
+gridStep(stage,grid);
+
+
+
+end
+
